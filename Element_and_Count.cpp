b714aa6701caee860ba62bd22b1f108b7d6d128c
navi0307 <97536148+navi0307@users.noreply.github.com>
2022-10-15 22:49:51 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     int arr[n];     vector<int>ans;     for(int i=0;i<n;i++){         cin>>arr[i];     }     unordered_map<int,int>map;     for(int i=0;i<n;i++){         map[arr[i]]++;         if(map[arr[i]]==1){         ans.push_back(arr[i]);     }     }     for(int i=0;i<ans.size();i++){         cout<<ans[i]<<" "<<map[ans[i]]<<" ";     } }

---
+++ Element_and_Count.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    unordered_map<int,int>map;
+    for(int i=0;i<n;i++){
+        map[arr[i]]++;
+        if(map[arr[i]]==1){
+        ans.push_back(arr[i]);
+    }
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" "<<map[ans[i]]<<" ";
+    }
+}
